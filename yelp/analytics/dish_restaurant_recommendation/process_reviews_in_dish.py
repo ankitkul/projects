5b65517dd28b8d7bf5732b0d5ce0b21a919b9c93
Ankit Kulshreshtha <ankit.kulshreshtha@gmail.com>
2017-08-13 17:15:40 -0700

    Mining top restaurants by dish

--- yelp/analytics/dish_restaurant_recommendation/process_reviews_in_dish.py
+++ yelp/analytics/dish_restaurant_recommendation/process_reviews_in_dish.py
@@ -6,6 +6,58 @@ path2files="../task1/yelp_dataset_challenge_academic_dataset/"
 path2buisness=path2files+"yelp_academic_dataset_business.json"
 path2reviews=path2files+"yelp_academic_dataset_review.json"
 
+def top_restaurants_for_dish():
+	with open ("output/Indian.json" , 'r') as f:
+		reviews = json.load(f)
+
+	with open ("output/review_rating_indian.json" , 'r') as f:
+		review_rating = json.load(f)
+
+	with open ("output/reviewid_rid.json" , 'r') as f:
+		review2rid = json.load(f)
+
+	with open ("output/business_details_indian.json", 'r') as f:
+	    business_json = json.load(f)
+
+	restaurant_review_rating = {}
+	dish_name = ['garlic naan','chicken tikka masala','tandoori chicken','mango lassi','lamb vindaloo','vegetable samosas']
+
+	for item in dish_name:
+		for key, value in reviews.iteritems():
+			if item in value:
+				id = review2rid[key]
+				if id in restaurant_review_rating:
+					restaurant_review_rating[id].append(int(review_rating[key]))
+				else:
+					restaurant_review_rating[id] = [int(review_rating[key])]
+		
+		popular_restaurant =[]				
+		for k, v in restaurant_review_rating.iteritems():
+			total_rating = 0
+			for rating in v:
+				total_rating += rating
+
+			avg_rating = total_rating * 1 /len(v)
+			freq = len(v)
+			r_name = business_json[k]["name"]
+			city = business_json[k]["city"]
+			state = business_json[k]["state"]
+			stars = business_json[k]["stars"]
+
+			if freq > 5:
+				sudo_rating = avg_rating**2
+			else:
+				sudo_rating = avg_rating**(1/2)
+			score = freq * sudo_rating
+
+			popular_restaurant.append((r_name, score, avg_rating, freq, city, state, stars))
+		
+		with open('output/popular_restaurant_' + item + '.csv','wb') as out:
+		    csv_out=csv.writer(out)
+		    csv_out.writerow(['id','value','rating','freq','city','state','stars'])
+		    for row in popular_restaurant:
+		        csv_out.writerow(row)
+
 def top_dishes():
 	top_dishes = []
 	with open('data/original_top-dishes.csv') as csvfile:
@@ -72,6 +124,7 @@ def generate_data():
 	#ensure categories is a directory
 	sample_cat2reviews={}
 	sample_cat2ratings={}
+	review2businessid = {}
 	business_id = {}
 	num_reviews = 0
 	with open (path2reviews, 'r') as f:
@@ -83,17 +136,28 @@ def generate_data():
 				    num_reviews = num_reviews + 1
 				    sample_cat2reviews[num_reviews] = review_json['text']
 				    sample_cat2ratings[num_reviews] = str(review_json['stars'])
-
+				    review2businessid[num_reviews] = rid
 
 				if rid in business_id:
 					business_id[rid] += 1
 				else:
 					business_id[rid] = 1
 
-	print num_reviews
+	print "reading from business file..."
+	business_details = {}
+	with open (path2buisness, 'r') as f:
+	    for line in f.readlines():
+	        business_json = json.loads(line)
+	        rid = business_json['business_id']
+	        if rid in business_id:
+				business_details[rid] = business_json
+
+	print "# of reviews: %i" % num_reviews
 	print len(business_id)    
 	print len(sample_cat2reviews)
 	print len(sample_cat2ratings)
+	print len(review2businessid)
+	print "Business details for %i" % len(business_details)
 
 	print "saving categories"
 	with open('output/Indian.json', 'w') as fp:
@@ -105,7 +169,17 @@ def generate_data():
 
 	print "saving business_id"
 	with open('output/rid_indian.json', 'w') as fp:
-		json.dump(business_id, fp)        
+		json.dump(business_id, fp)   
 
-generate_data()
-#top_dishes()        
+	print "saving review id -> restaurant id"
+	with open('output/reviewid_rid.json', 'w') as fp:
+		json.dump(review2businessid, fp)
+
+	print "saving business details"
+	with open('output/business_details_indian.json', 'w') as fp:
+		json.dump(business_details, fp)
+
+
+#generate_data()
+#top_dishes() 
+top_restaurants_for_dish()
